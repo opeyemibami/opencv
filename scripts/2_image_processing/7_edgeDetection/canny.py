73a245f977a641115fbde67b9b63e48dba3aa7de
opeyemibami <bamigbadeopeyemi@gmail.com>
2024-01-05 05:47:47 +0000

    edge detection

---
+++ scripts/2_image_processing/7_edgeDetection/canny.py
@@ -0,0 +1,23 @@
+import cv2 as cv 
+import pathlib 
+import numpy as np
+
+base_dir = pathlib.Path(__file__).parents[3]
+img_path = str(base_dir.joinpath("data","yhemmy1.jpg"))
+# read image
+img = cv.imread(img_path)
+
+# edge detection
+img_edge = cv.Canny(img,130,170)
+
+# dilate
+img_edge_dilate = cv.dilate(img_edge,np.ones((3,3),dtype=np.int8))
+
+# Erode 
+img_edge_erode = cv.erode(img_edge_dilate,np.ones((3,3),dtype=np.int8))
+
+cv.imshow("img",img)
+cv.imshow("img_edge",img_edge)
+cv.imshow("img_edge_dilate",img_edge_dilate)
+cv.imshow("img_edge_erode",img_edge_erode)
+cv.waitKey(0)
---
+++ scripts/2_image_processing/7_edgeDetection/readme.md
@@ -0,0 +1,22 @@
+# Title: Image processing basics
+
+# Knowledge
+
+- ## Brief explanation
+   
+- ## Psuedocode / Major Steps
+
+  Canny Edge Detection:
+
+  - import libraries
+  - define image path
+  - read image
+  - detect edges image
+  - (optional) dilate edges or erode dilated edges 
+  - visualize detected edges in image
+  - set waitkey
+
+- ## Use cases and scenario
+  
+- ## Techniques
+- ## Alternatives Techniques
